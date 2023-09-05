c55842d14afb85456f0034421bc2d8496d53d3a4
Dany Zaky <63490050+DanyZaky@users.noreply.github.com>
2023-09-05 19:21:17 +0700

    target marker

--- Assets/Scripts/Enemy/EnemyDetection.cs
+++ Assets/Scripts/Enemy/EnemyDetection.cs
@@ -5,19 +5,21 @@ public class EnemyDetection : MonoBehaviour
 {
     [SerializeField] private EnemyManager enemyManager;
     private MovementInput movementInput;
-    private CombatScript combatScript;
 
     public LayerMask layerMask;
 
     [SerializeField] Vector3 inputDirection;
     [SerializeField] private EnemyScript currentTarget;
 
+    [SerializeField] private GameObject enemyTargetMarker;
+    public int targetIndex;
+
     public GameObject cam;
 
     private void Start()
     {
         movementInput = GetComponentInParent<MovementInput>();
-        combatScript = GetComponentInParent<CombatScript>();
+        targetIndex = 0;
     }
 
     private void Update()
@@ -34,13 +36,46 @@ public class EnemyDetection : MonoBehaviour
 
         inputDirection = forward * movementInput.moveAxis.y + right * movementInput.moveAxis.x;
         inputDirection = inputDirection.normalized;
-
+        /*
         RaycastHit info;
 
         if (Physics.SphereCast(transform.position, 3f, inputDirection, out info, 10,layerMask))
         {
             if(info.collider.transform.GetComponent<EnemyScript>().IsAttackable())
                 currentTarget = info.collider.transform.GetComponent<EnemyScript>();
+        }*/
+
+        if(Input.GetKeyDown(KeyCode.Tab))
+        {
+            targetIndex += 1;
+            if (targetIndex >= enemyManager.allEnemies.Length)
+            {
+                targetIndex = 0;
+            }
+
+            if(enemyManager.allEnemies[targetIndex].enemyAvailability == true)
+            {
+                currentTarget = enemyManager.allEnemies[targetIndex].enemyScript;
+            }
+            else
+            {
+                targetIndex += 1;
+            }
+        }
+
+        SetMarketTarget();
+    }
+
+    private void SetMarketTarget()
+    {
+        if (CurrentTarget() != null)
+        {
+            enemyTargetMarker.transform.position = new Vector3(CurrentTarget().transform.position.x, CurrentTarget().transform.position.y + 2, CurrentTarget().transform.position.z);
+            enemyTargetMarker.SetActive(true);
+        }
+        else
+        {
+            enemyTargetMarker.SetActive(false);
         }
     }
 
@@ -61,7 +96,7 @@ public class EnemyDetection : MonoBehaviour
 
     private void OnDrawGizmos()
     {
-        Gizmos.color = Color.black;
+        Gizmos.color = Color.red;
         Gizmos.DrawRay(transform.position, inputDirection);
         Gizmos.DrawWireSphere(transform.position, 1);
         if(CurrentTarget() != null)
